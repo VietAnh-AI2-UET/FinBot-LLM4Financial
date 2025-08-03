2a48fcb456840395ecd3cbb37e4ac8529a8a6242
VietAnh <vietanhtb057@gmail.com>
2025-08-03 09:35:01 +0700

    update code: change function and variable name

--- core/docx_table_reader.py
+++ core/docx_table_reader.py
@@ -1,23 +1,23 @@
 from docx import Document
 import pandas as pd
 
-def read_docx_file(path) -> Document:
+def get_document(path) -> Document:
     document = Document(path)
     return document
 
 #return the informations list of indicated table
-def extract_table_info(tables, index) -> list:
+def extract_table_info(tables, table_index) -> list:
     table_infos = []
-    table = tables[index]
+    table = tables[table_index]
     for row in table.rows:
         row_contents = [cell.text for cell in row.cells]
         table_infos.append(row_contents)
     return table_infos
 
-def get_table_dataframe(path, index):
+def get_table_dataframe(path, table_index):
     try:
         path = path                #change the "path" according to yours
-        document = read_docx_file(path=path)
+        document = get_document(path=path)
         print('file located')
 
     except Exception as e:
@@ -31,12 +31,13 @@ def get_table_dataframe(path, index):
     except Exception as e:
         print('cant extract table from file')
 
-    #todo: extract certain table by index
+    #todo: extract certain table by table_index
     try:
-        if index < 0 or index >= len(tables):
-            print('Invalid index')
+        if table_index < 0 or table_index >= len(tables):
+            print('Invalid table_index')
+            return None
         else:
-            table_infos = extract_table_info(tables=tables, index=index)
+            table_infos = extract_table_info(tables=tables, table_index=table_index)
             df = pd.DataFrame(table_infos)
             return df
 
