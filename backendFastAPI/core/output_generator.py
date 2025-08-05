1fd809216ffbd0dc149c02d244fb94a1f9a42d56
anhduc1234567 <ducanhnguyen.work@gmail.com>
2025-08-05 11:30:08 +0700

    upload ui

--- backendFastAPI/core/output_generator.py
+++ backendFastAPI/core/output_generator.py
@@ -45,7 +45,7 @@ def respond_user(user_question,temp_path):
 
     # client = InferenceClient (
     #     provider="nebius",
-    #   thêm api vào
+   
     # )
 
     # completion = client.chat.completions.create(
