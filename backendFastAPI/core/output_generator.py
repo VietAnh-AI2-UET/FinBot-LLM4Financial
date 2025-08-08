b6450c25ce80670e392f5e34d8ef71ad6765be12
anhduc1234567 <ducanhnguyen.work@gmail.com>
2025-08-08 20:57:52 +0700

    use Genimi

--- backendFastAPI/core/output_generator.py
+++ backendFastAPI/core/output_generator.py
@@ -1,6 +1,8 @@
 from core.receiver import find_information
 from huggingface_hub import InferenceClient
-
+from google import genai
+import os
+GOOGLE_API = "" #https://aistudio.google.com/apikey truy cập để lấy API
 #get user question and similar information -> create a prompt for LLM
 def get_user_prompt(input_model='all-MiniLM-L6-v2', num_sim_docx=5, user_question = '',temp_path = None) ->str:
     user_question, similar_info = find_information(input_model=input_model, k=num_sim_docx,user_question=user_question,temp_path=temp_path)
@@ -22,26 +24,9 @@ def get_user_prompt(input_model='all-MiniLM-L6-v2', num_sim_docx=5, user_questio
 
 def respond_user(user_question,temp_path):
     user_prompt = get_user_prompt(input_model='all-MiniLM-L6-v2', num_sim_docx=5, user_question=user_question,temp_path=temp_path)
-
-    client = InferenceClient (
-        provider="nebius",
-        # them api key để chạy
+    client = genai.Client(api_key= GOOGLE_API)
+    os.environ["GEMINI_API_KEY"] = GOOGLE_API
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=user_prompt
     )
-
-    completion = client.chat.completions.create(
-        model="deepseek-ai/DeepSeek-R1-0528",
-        messages=[
-            {
-                'role': "system",
-                'content': 'Bạn là một trợ lý AI có khả năng truy xuất thông tin trong tài liệu mà người dùng cung cấp. Từ những thông tin đó, bạn trả lời câu hỏi mà người dùng đưa ra nếu câu hỏi không có trong tài liệu hãy nói thẳng '
-            },
-
-            {
-                "role": "user",
-                "content": user_prompt
-            }
-        ],
-    )
-    
-    # print(user_prompt)
-    return (completion.choices[0].message.content)
+    return response.text
